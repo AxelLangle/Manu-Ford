95c2205e1533ab37159a6cab1854065f55567157
Ricardo Villarreal Moreno <ricardo_1323144045@uptecamac.edu.mx>
2025-11-13 17:29:49 -0600

    feat: Implementacion de la página de inicio de sesión

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,19 +1,29 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/react';
 import { PropsWithChildren } from 'react';
+import logoFord from '../../assets/images/logoFord.jpg';
 
-export default function Guest({ children }: PropsWithChildren) {
+type GuestProps = PropsWithChildren<{ noCard?: boolean }>;
+
+export default function Guest({ children, noCard }: GuestProps) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
-                </Link>
-            </div>
+        <div className="flex min-h-screen flex-col items-center bg-white pt-20 sm:pt-24 sm:justify-center">
+            <header className="w-full absolute top-0 left-0 z-20">
+                <div className="w-full">
+                    <div className="flex items-start py-2 pl-0">
+                        <Link href="/">
+                            <img src={logoFord} alt="Ford" className="h-16 sm:h-20 object-contain ml-4 mt-2"  />
+                        </Link>
+                    </div>
+                </div>
+            </header>
 
-            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
-                {children}
-            </div>
+            {noCard ? (
+                <div className="w-full">{children}</div>
+            ) : (
+                <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+                    {children}
+                </div>
+            )}
         </div>
     );
 }
